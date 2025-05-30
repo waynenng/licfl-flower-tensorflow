767d5dfc1ee0735534d3cf8500d15c78cf3246cf
ngzuwayne <140599260+ngzuwayne@users.noreply.github.com>
2025-05-30 17:23:46 +0800

    Added 11 other metrics

--- licfl/client_app.py
+++ licfl/client_app.py
@@ -5,6 +5,17 @@ from flwr.common import Context
 
 from licfl.task import load_data, load_model
 
+import time
+import numpy as np
+from sklearn.metrics import (
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    mean_absolute_error,
+    mean_squared_error,
+    mean_absolute_percentage_error,
+)
+
 
 # Define Flower Client and client_fn
 class FlowerClient(NumPyClient):
@@ -30,8 +41,57 @@ class FlowerClient(NumPyClient):
 
     def evaluate(self, parameters, config):
         self.model.set_weights(parameters)
+
+        # Inference time
+        start_infer = time.time()
+        y_pred_probs = self.model.predict(self.x_test, verbose=0)
+        end_infer = time.time()
+
+        # Convert to class labels (assuming binary classification or categorical softmax)
+        if y_pred_probs.shape[1] == 1:
+            y_pred = (y_pred_probs > 0.5).astype(int)
+        else:
+            y_pred = np.argmax(y_pred_probs, axis=1)
+
+        y_true = self.y_test
+
+        # Accuracy from model.evaluate (optional but kept for parity)
+        start_train = time.time()
         loss, accuracy = self.model.evaluate(self.x_test, self.y_test, verbose=0)
-        return loss, len(self.x_test), {"accuracy": accuracy}
+        end_train = time.time()
+
+        # Basic Metrics
+        recall = recall_score(y_true, y_pred, average="macro", zero_division=0)
+        f1 = f1_score(y_true, y_pred, average="macro", zero_division=0)
+        auc = roc_auc_score(y_true, y_pred_probs, multi_class="ovr") if y_pred_probs.shape[1] > 1 else roc_auc_score(y_true, y_pred_probs)
+        
+        # Error Metrics
+        mae = mean_absolute_error(y_true, y_pred)
+        mape = mean_absolute_percentage_error(y_true, y_pred)
+        mse = mean_squared_error(y_true, y_pred)
+        rmse = np.sqrt(mse)
+
+        # Model efficiency metrics
+        param_count = self.model.count_params()
+        flops = 0  # Set to 0 or compute if you have a FLOP profiler
+        inference_time = end_infer - start_infer
+        training_time = end_train - start_train
+
+        return loss, len(self.x_test), {
+            "accuracy": accuracy,
+            "recall": recall,
+            "f1": f1,
+            "auc": auc,
+            "mae": mae,
+            "mape": mape,
+            "mse": mse,
+            "rmse": rmse,
+            "parameters": param_count,
+            "flops": flops,
+            "inference_time": inference_time,
+            "training_time": training_time,
+        }
+
 
 
 def client_fn(context: Context):
--- licfl/server_app.py
+++ licfl/server_app.py
@@ -7,15 +7,22 @@ from flwr.server.strategy import FedAvg
 from licfl.task import load_model
 
 
-def aggregate_accuracy(metrics):
+def aggregate_metrics(metrics):
     total_examples = 0
-    total_accuracy = 0.0
+    summed_metrics = {
+        "accuracy": 0.0, "recall": 0.0, "f1": 0.0, "auc": 0.0,
+        "parameters": 0.0, "flops": 0.0, "inference_time": 0.0, "training_time": 0.0,
+        "mae": 0.0, "mape": 0.0, "rmse": 0.0, "mse": 0.0,
+    }
 
     for num_examples, metric_dict in metrics:
         total_examples += num_examples
-        total_accuracy += num_examples * metric_dict["accuracy"]
+        for k in summed_metrics.keys():
+            if k in metric_dict:
+                summed_metrics[k] += num_examples * metric_dict[k]
 
-    return {"accuracy": total_accuracy / total_examples}
+    averaged_metrics = {k: v / total_examples for k, v in summed_metrics.items()}
+    return averaged_metrics
 
 def server_fn(context: Context):
     # Read from config
@@ -25,12 +32,12 @@ def server_fn(context: Context):
     parameters = ndarrays_to_parameters(load_model().get_weights())
 
     # Define strategy
-    strategy = strategy = FedAvg(
+    strategy = FedAvg(
         fraction_fit=1.0,
         fraction_evaluate=1.0,
         min_available_clients=2,
         initial_parameters=parameters,
-        evaluate_metrics_aggregation_fn=aggregate_accuracy,
+        evaluate_metrics_aggregation_fn=aggregate_metrics,
     )
     config = ServerConfig(num_rounds=num_rounds)
 
